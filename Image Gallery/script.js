6522a1e811e316bc0715905f7b55255ada403133
Sahil <sahilcodeprogramming@gmail.com>
2025-09-20 10:20:10 +0530

    images name update

--- Image Gallery/script.js
+++ Image Gallery/script.js
@@ -1,24 +1,24 @@
 const supercars = [
-    { name: "Ferrari LaFerrari", image: "ferrari laferrari.jpg" },
-    { name: "Bugatti Chiron", image: "bugatti chiron.jpg" },
-    { name: "Lamborghini Aventador", image: "lamborghini aventador.jpg" },
-    { name: "McLaren P1", image: "mclaren p1.jpg" },
-    { name: "Porsche 918 Spyder", image: "porsche 918 spyder.jpg" },
-    { name: "Koenigsegg Jesko", image: "koenigsegg jesko.jpg" },
-    { name: "Aston Martin Valkyrie", image: "aston martin valkyrie.jpg" },
-    { name: "Pagani Huayra", image: "pagani huayra.jpg" },
-    { name: "Rimac C Two", image: "rimac c_two.jpg" },
-    { name: "Ferrari 812 Superfast", image: "ferrari 812 superfast.jpg" },
-    { name: "Lamborghini Sián", image: "lamborghini sián.jpg" },
-    { name: "McLaren Speedtail", image: "mclaren speedtail.jpg" },
-    { name: "Audi R8 V10", image: "audi r8 v10.jpg" },
+    { name: "Ferrari LaFerrari", image: "Ferrari LaFerrari.jpg" },
+    { name: "Bugatti Chiron", image: "Bugatti Chiron.jpg" },
+    { name: "Lamborghini Aventador", image: "Lamborghini Aventador.jpg" },
+    { name: "McLaren P1", image: "McLaren P1.jpg" },
+    { name: "Porsche 918 Spyder", image: "Porsche 918 Spyder.jpg" },
+    { name: "Koenigsegg Jesko", image: "Koenigsegg Jesko.jpg" },
+    { name: "Aston Martin Valkyrie", image: "Aston Martin Valkyrie.jpg" },
+    { name: "Pagani Huayra", image: "Pagani Huayra.jpg" },
+    { name: "Rimac C Two", image: "Rimac C_Two.jpg" },
+    { name: "Ferrari 812 Superfast", image: "Ferrari 812 Superfast.jpg" },
+    { name: "Lamborghini Sián", image: "Lamborghini Sián.jpg" },
+    { name: "McLaren Speedtail", image: "McLaren Speedtail.jpg" },
+    { name: "Audi R8 V10", image: "Audi R8 V10.jpg" },
     { name: "Nissan GT-R Nismo", image: "Nissan GT-R Nismo.jpg" },
-    { name: "Chevrolet Corvette ZR1", image: "chevrolet corvette zr1.jpg" },
-    { name: "Maserati MC20", image: "maserati mc20.jpg" },
-    { name: "Lotus Evija", image: "lotus evija.jpg" },
-    { name: "BMW i8", image: "bmw i8.jpg" },
-    { name: "Ford GT", image: "ford gt.jpg" },
-    { name: "Jaguar C X75", image: "jaguar c-x75.jpg" }
+    { name: "Chevrolet Corvette ZR1", image: "Chevrolet Corvette ZR1.jpg" },
+    { name: "Maserati MC20", image: "Maserati MC20.jpg" },
+    { name: "Lotus Evija", image: "Lotus Evija.jpg" },
+    { name: "BMW i8", image: "BMW i8.jpg" },
+    { name: "Ford GT", image: "Ford GT.jpg" },
+    { name: "Jaguar C X75", image: "Jaguar C-X75.jpg" }
 ];
 const gallery = document.getElementById('gallery');
 
@@ -32,4 +32,5 @@ for (let car = 0; car < supercars.length; car++) {
     `;
 
 }
+
 
