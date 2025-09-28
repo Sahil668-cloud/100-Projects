9ed5e720d5e06e6bb15e42248f1af93e1e3e3569
Sahil <sahilcodeprogramming@gmail.com>
2025-09-28 16:13:33 +0530

    Add files via upload

---
+++ Responsive Landing Page/script.js
@@ -0,0 +1,7 @@
+// ✅ JS Toggle for Mobile
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
